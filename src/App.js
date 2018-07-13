5a90a2a56cc607538f11f3ee619bffff28651424
DESKTOP-VADE9II\hoduc <trunghd1231@gmail.com>
2018-07-13 22:33:10 +0700

    complete login page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route} from 'react-router-dom';
 import Homepage from "./components/pages/Homepage";
 import LoginPage from "./components/pages/LoginPage";
 
-const App = () => <div>
+const App = () => <div className="ui container">
   <Route path="/" exact component={Homepage} />
   <Route path="/login" exact component={LoginPage} />
 </div>;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LoginForm from '../forms/LoginForm';
 
 export default class LoginPage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
-    )
-  }
+    submit = data => {
+        console.log(data);
+    };
+    render() {
+        return ( 
+            <div>
+                <h1> Login Page </h1> 
+                <LoginForm submit={ this.submit } /> 
+            </div>
+        )
+    }
 }
